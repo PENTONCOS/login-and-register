bcf2315bb96f1a291eaf78f7373ba8320b2bbb66
jiapandong <jiapandong@supconit.com>
2023-06-25 15:53:20 +0800

    feat: done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3308,
+      username: "root",
+      password: "jiapandong",
+      database: "login_test",
+      synchronize: true,
+      logging: true,
+      entities: [User],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      }
+    }),
+    JwtModule.register({
+      global: true,
+      secret: 'jiapandong',
+      signOptions: {
+        expiresIn: '7d'
+      }
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
