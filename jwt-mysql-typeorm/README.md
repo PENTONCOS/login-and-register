e00aa2f2b807d0ccb805dae0c1c00534e01989e1
jiapandong <jiapandong@supconit.com>
2023-07-04 17:24:57 +0800

    feat: seesion + redis 实现 ACL 权限控制

--- jwt-mysql-typeorm/README.md
+++ jwt-mysql-typeorm/README.md
@@ -1,2 +1,2 @@
-# Nest + MySQL + TypeORM
-使用 Nest + MySQL + TypeORM 实现的登录注册模块
+# jwt-mysql-typeorm
+使用 `jwt` 实现的登录注册
--- seesion-mysql-typeorm/src/app.module.ts
+++ seesion-mysql-typeorm/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Permission } from './user/entities/permission.entity';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3308,
+      username: "root",
+      password: "jiapandong",
+      database: "acl_test",
+      synchronize: true,
+      logging: true,
+      entities: [User, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      }
+    }),
+    UserModule, AaaModule, BbbModule, RedisModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
